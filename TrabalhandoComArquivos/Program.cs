4adecb704a38959782340536b8ce6e4770bd57d6
Angelo Carvalho <angelosuporteti@gmail.com>
2020-10-02 11:18:12 -0300

    FileStream e StreamReader - codigo otimizado

--- TrabalhandoComArquivos/Program.cs
+++ TrabalhandoComArquivos/Program.cs
@@ -8,13 +8,11 @@ namespace TrabalhandoComArquivos
         static void Main(string[] args)
         {
             string path = @"c:\teste\file1.txt";
-            FileStream fs = null;
             StreamReader sr = null;
             
             try
             {
-                fs = new FileStream(path, FileMode.Open);
-                sr = new StreamReader(fs);
+                sr = File.OpenText(path);
                 string line = sr.ReadLine();
                 Console.WriteLine(line);
                 Console.WriteLine("Press enter to continue");
@@ -26,11 +24,10 @@ namespace TrabalhandoComArquivos
                 Console.WriteLine(e.Message);
             }
 
-            // Como o uso do FileStream e StreamReader está ligado a diálogo com o SO, vamos usar o bloco abaixo para fechamento manual
+            
             finally
             {
                 if (sr != null) sr.Close();
-                if (fs != null) fs.Close();
             }
         }
     }
